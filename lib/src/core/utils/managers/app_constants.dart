b6ec4293364703fa1d2443deb0157185681c6ba6
Syf-Almjd <syfalmjd11@gmail.com>
2025-07-27 19:47:21 +0200

    🎯 init: clean internal files setup

--- lib/src/core/utils/managers/app_constants.dart
+++ lib/src/core/utils/managers/app_constants.dart
@@ -1,10 +1,7 @@
-import 'app_enums.dart';
-
 abstract class AppConstants {
   static const String appTitle = "Flutter Application";
   static const String appVersion = "V1.0.0";
   static const String noPhoto = "NOPHOTO";
   static const String appFontFamily = "poppins";
   static const int numOfItemsInNews = 30;
-  AppLocale appDefaultLocale = AppLocale.english;
 }
--- lib/src/core/utils/managers/app_strings.dart
+++ lib/src/core/utils/managers/app_strings.dart
@@ -1,4 +1,6 @@
 abstract class AppSocialLinks {
-  static const String playStoreURL = "https://www.latipaagency.it/";
-  static const String appleStoreURL = "https://www.latipaagency.it/";
+  static const String playStoreURL =
+      "https://github.com/Syf-Almjd/flutter_clean_coded/tree/main";
+  static const String appleStoreURL =
+      "https://github.com/Syf-Almjd/flutter_clean_coded/tree/main";
 }
